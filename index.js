b5144f6dc87c229ecbefa0427062b126c93bcd83
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2024-02-02 15:56:59 -0500

    had an issue with my node modules not ignoring, removing and installing again

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 //  Runs the application using imports from lib/
+const inquirer = require('inquirer');
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Please enter your text(up to 3 characters).',
+        },
+        {
+            type: 'input',
+            name: 'text-color',
+            message: 'Please choose your text color.',
+        },
+        // {
+        //     type: 'list',
+        //     name: 'shape',
+        //     message: ['circle', 'triangle', 'square'],
+        // },
+        {
+            type: 'input',
+            name: 'shape-color',
+            message: 'Please choose your shape color.',
+        },
+    ])
