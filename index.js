1d612bf5e14c476b1620b2aec9eda07fc7eec4ba
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2024-02-03 15:43:38 -0500

    created an instance of the user's chosen shape

--- index.js
+++ index.js
@@ -4,29 +4,43 @@ const { default: Choices } = require('inquirer/lib/objects/choices');
 const fs = require('fs');
 const {Circle, Triangle, Square} = require("./lib/shapes");
 
-
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'text',
-            message: 'Please enter your text(up to 3 characters).',
-        },
-        {
-            type: 'input',
-            name: 'text-color',
-            message: 'Please choose your text color using a color keyword or hexadecimal number.',
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Please choose a shape.',
-            choices: ['Circle', 'Triangle', 'Square'],
-
-        },
-        {
-            type: 'input',
-            name: 'shape-color',
-            message: 'Please choose your shape color using a color keyword or hexadecimal number.',
-        },
-    ])
+function generateLogo(){
+    inquirer
+        .prompt([
+            {
+              type: 'input',
+              name: 'text',
+              message: 'Please enter your text(up to 3 characters).',
+            },
+            {
+              type: 'input',
+              name: 'text-color',
+              message: 'Please choose your text color using a color keyword or hexadecimal number.',
+            },
+            {
+              type: 'list',
+              name: 'shape',
+              message: 'Please choose a shape.',
+              choices: ['Circle', 'Triangle', 'Square'],
+            },
+            {
+              type: 'input',
+              name: 'shape-color',
+              message: 'Please choose your shape color using a color keyword or hexadecimal number.',
+            },
+        ])
+        .then((answers) => {
+            let logoShape;
+            switch(answers.shape) {
+              case 'Circle':
+                logoShape = new Circle();
+                break;
+              case 'Triangle':
+                logoShape = new Triangle();
+                break;
+              case 'Square':
+                logoShape = new Square();
+                break;
+            }
+        })
+};
