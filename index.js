b54c59287bde7f6aa569bac3627a27c1aa181bba
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2024-02-04 10:08:57 -0500

    cleaned up code, test functions properly as well as logo generater

--- index.js
+++ index.js
@@ -1,53 +1,3 @@
-//  Runs the application using imports from lib/
-// const inquirer = require('inquirer');
-// const { default: Choices } = require('inquirer/lib/objects/choices');
-// const fs = require('fs');
-// const {Circle, Triangle, Square} = require("./lib/shapes");
-
-// function generateLogo(){
-//     inquirer
-//         .prompt([
-//             {
-//               type: 'input',
-//               name: 'text',
-//               message: 'Please enter your text(up to 3 characters).',
-//             },
-//             {
-//               type: 'input',
-//               name: 'text-color',
-//               message: 'Please choose your text color using a color keyword or hexadecimal number.',
-//             },
-//             {
-//               type: 'list',
-//               name: 'shape',
-//               message: 'Please choose a shape.',
-//               choices: ['Circle', 'Triangle', 'Square'],
-//             },
-//             {
-//               type: 'input',
-//               name: 'shape-color',
-//               message: 'Please choose your shape color using a color keyword or hexadecimal number.',
-//             },
-//         ])
-//         .then((answers) => {
-//             let logoShape;
-//             switch(answers.shape) {
-//               case 'Circle':
-//                 logoShape = new Circle();
-//                 break;
-//               case 'Triangle':
-//                 logoShape = new Triangle();
-//                 break;
-//               case 'Square':
-//                 logoShape = new Square();
-//                 break;
-//             }
-//         })
-// };
-// const inquirer = require('inquirer');
-// const fs = require('fs');
-// const { Circle, Triangle, Square } = require('./lib/shapes');
-
 const inquirer = require('inquirer');
 const { default: Choices } = require('inquirer/lib/objects/choices');
 const fs = require('fs');
@@ -60,6 +10,7 @@ function generateLogo() {
         type: 'input',
         name: 'text',
         message: 'Please enter your text (up to 3 characters).',
+        // validate is making sure only 3 characters are input, and will error if more than 3 are input
         validate: function(input){
         if (input.trim().length === 0 || input.length > 3) {
           return "Please enter up to 3 characters."
@@ -106,7 +57,7 @@ function generateLogo() {
       const fileName = 'logo.svg';
       const filePath = `examples/${fileName}`;
 
-      // Create examples directory if it doesn't exist
+      // creates "examples" directory if it doesn't exist
       if (!fs.existsSync('examples')) {
         fs.mkdirSync('examples');
       }
